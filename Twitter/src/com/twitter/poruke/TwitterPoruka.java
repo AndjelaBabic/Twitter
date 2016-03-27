ec6bef90e09224f48d76916c17d0f6439dfb1628
AndjelaBabic <andjelasbabic@gmail.com>
2016-03-27 01:15:17 +0100

    Uradjena dokumentacija za klasu TwitterPoruka

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,29 +1,56 @@
 package com.twitter.poruke;
-
+/**
+ * 
+ * @author Andjela
+ * Klasa se odnosi na twitter poruku
+ */
 public class TwitterPoruka {
+	/**
+	 * Informacije o korisniku poruke
+	 */
 	private String korisnik;
+	/**
+	 * Sadrzi poruku koju korisnik salje
+	 */
 	private String poruka;
-
+	/**
+	 * Metoda vraca korisnika poruke
+	 * @return Korisnik kao string
+	 */
 	public String getKorisnik() {
 		return korisnik;
 	}
-
+	/**
+	 * Metoda postavlja korisnika poruke
+	 * @param korisnik username korisnika u vidu stringa
+	 * @throws java.lang.RuntimeException Ukoliko je ime null ili prazan string
+	 */
 	public void setKorisnik(String korisnik) {
 		if (korisnik == null || korisnik == "")
 			throw new RuntimeException("Ime korisnika mora biti uneto");
 		this.korisnik = korisnik;
 	}
-
+	/**
+	 * Metoda vraca poruku
+	 * @return poruka kao string
+	 */
 	public String getPoruka() {
 		return "poruka";
 	}
-
+	/**
+	 * Metoda postavlja poruku
+	 * @param poruka poruka korisnika u vidu stringa
+	 * @throws java.lang.RuntimeException Ukoliko poruka nije uneta ili ima vise od 140 znakova
+	 */
 	public void setPoruka(String poruka) {
 		if (poruka == null || this.poruka.length() > 140)
 			throw new RuntimeException("Poruka mora biti uneta i mora imati najvise 140 znakova");
 		this.poruka = poruka;
 	}
-
+	/**
+	 * Metoda vraca informacije o korisniku i poruci
+	 * @return korisnik i poruka kao string
+	 */
 	public String toString() {
 		return "KORISNIK:" + korisnik + " PORUKA:" + poruka;
 	}
