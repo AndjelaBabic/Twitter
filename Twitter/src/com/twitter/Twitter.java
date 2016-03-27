ccfcdcf15cf7a0d4d607b628968ff09a1aad14c8
AndjelaBabic <andjelasbabic@gmail.com>
2016-03-27 11:27:12 +0100

    Dokumentacija za klasu Twitter, generisan JavaDoc, izmenjen .gitignore

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -2,14 +2,29 @@ package com.twitter;
 
 import java.util.LinkedList;
 import com.twitter.poruke.TwitterPoruka;
-
+/**
+ * 
+ * @author Andjela
+ * Twitter
+ */
 public class Twitter {
+	/**
+	 * Lista twitter poruka
+	 */
 	private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
-
+	/**
+	 * Metoda koja vraca listu poruka
+	 * @return poruke kao lista objekata tipa TwitterPoruka
+	 */
 	public LinkedList<TwitterPoruka> vratiSvePoruke() {
 		return poruke;
 	}
-
+	
+	/**
+	 * Metoda koja unosi novu poruku na kraj liste
+	 * @param korisnik Ime korisnika poruke
+	 * @param poruka Poruka koja se salje
+	 */
 	public void unesi(String korisnik, String poruka) {
 		// Pravi se nova poruka i puni podacima.
 		TwitterPoruka tp = new TwitterPoruka();
@@ -18,7 +33,14 @@ public class Twitter {
 		// Poruka se unosi u listu na kraj
 		poruke.addLast(tp);
 	}
-
+	/**
+	 * Metoda vraca niz objekata koji sadrzi poruke u kojima se nalazi odredjeni tag koji je
+	 * korisnik uneo
+	 * @param maxBroj Predstavlja kapacitet niza, maksimalan broj poruka 
+	 * @param tag Kljucna rec koju trazimo u nasim porukama
+	 * @return Vraca niz objekata klase TwitterPoruka
+	 * @throws java.lang.RuntimeException U slucaju da je tag null ili da je prazan
+	 */
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
 
 		if (tag == null || tag.isEmpty())
